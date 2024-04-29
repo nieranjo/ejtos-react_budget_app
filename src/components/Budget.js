a866ae976da58d7b9f569f4c34fa96cfeb95637a
nieranjo <nieranjo@icloud.com>
2024-04-29 11:48:27 -0400

    Step 6/8

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,16 +2,47 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    const [newBudget, setNewBudget] = useState(budget);
+    const { dispatch } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(0);
+    const [currency, setCurrency] = useState('£');
+
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
-    }
+        let value = parseInt(event.target.value);
+        
+        if (!isNaN(value)) {
+            setNewBudget(value);
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: value,
+            });
+        }
+    };
+
+    const handleCurrencyChange = (event) => {
+        setCurrency(event.target.value);
+    };
+
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div className='alert alert-secondary'>
+            <span>Budget: {currency}</span>
+            <input
+                type="number"
+                step="10"
+                value={newBudget}
+                onChange={handleBudgetChange}
+            />
+            <div className="currency">
+            <label htmlFor="currencySelect">Currency:</label>
+            <select value={currency} onChange={handleCurrencyChange} className="currencyChange">
+                <option value="$">$ Dollar</option>
+                <option value="£">£ Pound</option>
+                <option value="€">€ Euro</option>
+                <option value="₹">₹ Rupee</option>
+            </select>
+            </div>
+        </div>
     );
 };
+
 export default Budget;
+
